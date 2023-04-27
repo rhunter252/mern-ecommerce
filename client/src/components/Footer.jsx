f25bd476b916cca4d8f549953f2f4d0e69e7643f
Randy Hunter <randyh252@gmail.com>
2023-04-27 02:54:17 -0400

    navbar created and setup react router

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Icon,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
