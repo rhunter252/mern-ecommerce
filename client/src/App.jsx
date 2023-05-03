33c68556b1b0a5c4dd9a362e20ac22533dc61ce0
Randy Hunter <randyh252@gmail.com>
2023-05-03 19:41:38 -0400

    landing page and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,25 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import ProductsScreen from "./screens/ProductsScreen";
 import CartScreen from "./screens/CartScreen";
+import ProductScreen from "./screens/ProductScreen";
+import LandingScreen from "./screens/LandingScreen";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Navbar />
+        <LandingScreen />
         <main>
           <Routes>
+            <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/products" element={<ProductsScreen />} />
             <Route path="/cart" element={<CartScreen />} />
           </Routes>
+          <Footer />
         </main>
       </Router>
     </ChakraProvider>
--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,6 +1,11 @@
 import axios from "redaxios";
 
-import { setProducts, setLoading, setError } from "../slices/products";
+import {
+  setProducts,
+  setLoading,
+  setError,
+  setProduct,
+} from "../slices/products";
 
 export const getProducts = () => async (dispatch) => {
   dispatch(setLoading(true));
@@ -18,4 +23,60 @@ export const getProducts = () => async (dispatch) => {
       )
     );
   }
+};
+
+export const getProduct = (id) => async (dispatch) => {
+  dispatch(setLoading(true));
+  try {
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch(setProduct(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : "An unexpected error has occured. Please try again later."
+      )
+    );
+  }
+};
+
+export const createProductReview =
+  (productId, userId, comment, rating, title) => async (dispatch, getState) => {
+    dispatch(setLoading());
+    const {
+      user: { userInfo },
+    } = getState();
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        `/api/products/reviews/${productId}`,
+        { comment, userId, rating, title },
+        config
+      );
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      dispatch(productReviewed());
+    } catch (error) {
+      dispatch(
+        setError(
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+            ? error.message
+            : "An unexpected error has occured. Please try again later."
+        )
+      );
+    }
+  };
+
+export const resetProductError = () => async (dispatch) => {
+  dispatch(resetError());
 };
